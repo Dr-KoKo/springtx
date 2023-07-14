b771f7931b8514bd33e3fbcb95e3150034d5677f
Denny-Ko <donghar@naver.com>
2023-07-14 17:54:54 +0900

    스프링 트랜잭션 전파2 - 활용
    트랜잭션 전파 활용1 - 예제 프로젝트 시작

---
+++ src/main/java/hello/springtx/propagation/MemberRepository.java
@@ -0,0 +1,27 @@
+package hello.springtx.propagation;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class MemberRepository {
+    private final EntityManager em;
+
+    @Transactional
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Optional<Member> find(String username) {
+        return em.createQuery("select m from Member m where m.username=:username")
+                .setParameter("username", username)
+                .getResultStream().findAny();
+    }
+}
