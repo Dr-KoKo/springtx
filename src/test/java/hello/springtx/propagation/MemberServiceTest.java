f2fd8e0669c8faa2a62f2ce9eda254737c7bd876
Denny-Ko <donghar@naver.com>
2023-07-18 03:22:58 +0900

    스프링 트랜잭션 전파2 - 활용
    트랜잭션 전파 활용7 - 복구 REQUIRES_NEW

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -109,12 +109,28 @@ class MemberServiceTest {
     @Test
     void recoverException_fail() {
         //given
-        String username = "로그예외_outerTxOn_fail";
+        String username = "로그예외_recoverException_fail";
         //when
         Assertions.assertThatThrownBy(() -> memberService.joinV2(username))
                 .isInstanceOf(UnexpectedRollbackException.class);
         //then: 모든 데이터가 롤백된다.
         assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON(REQUIRES_NEW) Exception
+     */
+    @Test
+    void recoverException_success() {
+        //given
+        String username = "로그예외_recoverException_success";
+        //when
+        memberService.joinV2(username);
+        //then: 모든 데이터가 롤백된다.
+        assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isEmpty());
     }
 }
